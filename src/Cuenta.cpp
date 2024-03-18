04dd3411abfee5fd2f9f3704cc5afe386a1731bd
Jeffrey Alexander Argueta Galicia <argueta.galicia@outlook.com>
2024-03-18 14:25:16 -0600

    part2-trying

--- src/Cuenta.cpp
+++ src/Cuenta.cpp
@@ -1,9 +1,9 @@
 #include "../include/Cuenta.h"
 #define DA 5
 
-Cuenta::Cuenta() : cliente(Cliente(0,"","")), saldo(0) {
+Cuenta::Cuenta() : cliente(Cliente(0,"","")), saldo(0.00) {
     this->numeroCuenta = 0;
-    this->saldo = 0;
+    this->saldo = 0.00;
     this->contadorAbonos = 0;
 }
 
